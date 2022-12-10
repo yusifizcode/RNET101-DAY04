2ab2ae6e3b8e454b5ca032e2acbff56172c48bb0
yusifizcode <5ba0gh2@code.edu.az>
2022-12-10 09:51:25 +0400

    task updated

---
+++ HomeWork02-DAY04/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] arr = { 1, 2, 34, 67, 76, 12, 54 };
+
+
+// Task 1
+Array.ForEach(arr, i => Console.WriteLine(i));
+
+// Task 2
+Console.WriteLine("Bir eded daxil edin: ");
+
+// Way One
+
+//string numStr = Console.ReadLine();
+//if (arr.ToString().Contains(numStr))
+//    Console.WriteLine(true);
+//else
+//    Console.WriteLine(false);
+
+int num = Convert.ToInt32(Console.ReadLine());
+bool isExist = Array.Exists(arr, i => i == num);
+Console.WriteLine(isExist);
